4bd8629a8898777572cc388a3182f740dad28de4
Nora Beringer <56232123+noraberinger@users.noreply.github.com>
2024-08-16 23:25:49 +0200

    Removed node_modules and dist

--- packages/client/dist/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const App = () => {
-    const greeting = "Hello World";
-    const numberInput = 3;
-    return ((0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsxs)("h1", { children: [greeting, " is the Hello"] }), (0, jsx_runtime_1.jsx)("p", { children: numberInput })] }));
-};
-exports.default = App;
